436b5d11667b24adce3635a68a4dfeb790c9e223
kdcllc <info@kingdavidconsulting.com>
2021-02-10 23:46:09 -0500

    adding materialized styles

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# svelte-workshop
-Learning Svelte
+# Svelte Workshop
+
+Learning Svelte by problem solving.
+
+The main goal of this repo is to provide needed centralized location for getting staring with `Svelte` development.
+
+`svelte.svelte-vscode`
+
+
+* [Creating Svelte Form Control](./svelte-form-control/)
+
+
+## Documentation
+
+- [Svelte Comprehensive Article](./docs/svelte-article.pdf)
+- [Svelte Handbook](./docs/svelte-handbook.pdf)
+- [JavaScript Handbook](./docs/javascript-beginner-handbook.pdf)
+- [CSS Handbook](./docs/css-handbook.pdf)
--- svelte-form-control/src/App.svelte
+++ svelte-form-control/src/App.svelte
@@ -1,30 +1,48 @@
 <script>
-	export let name;
+  import { onMount, onDestroy  } from "svelte";
+  export let name;
+
+  let picker;
+
+  let d = '';
+
+  onMount(async () => {
+	var elem = document.querySelectorAll('.datepicker');
+    picker = M.Datepicker.getInstance(elem[0]);
+	d = picker.toString();
+  });
+
+  onDestroy(() => {
+		picker.destroy();
+	});
+
+  function handleDateChange(event) {
+    d = event.target.value;
+  }
+
+  $: console.log(picker)
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+
+  <div class="container">
+    <div class="row">
+      <input type="text" on:change={handleDateChange} class="datepicker" />
+    </div>
+    <div class="row">
+      <div class="col s12 m6">
+        <div class="card blue-grey darken-1">
+          <div class="card-content white-text">
+            <span class="card-title">Welcome</span>
+            <p>Hello {name}!</p>
+            <p>{d}</p>
+          </div>
+          <div class="card-action">
+            <a href="https://svelte.dev/tutorial">Svelte tutorial</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div />
+  </div>
 </main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
